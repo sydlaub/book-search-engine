5cb70bdb81680bb9a381fb47d59cb825be61541a
Sydney Laub <syd.laub18@gmail.com>
2023-08-28 21:35:12 -0400

    restructured files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,31 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from "@apollo/client/link/context";
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+
+
+// FUNCTIONS
+const httpLink = createHttpLink({ uri: "/graphql"});
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ... headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache({})
+});
+
 
 function App() {
   return (
